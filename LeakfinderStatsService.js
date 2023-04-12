77b319365a470e55dccabb5e9ce269cc513b8eb7
Foge65 <mr.texnik666@gmail.com>
2023-04-12 18:49:12 +0300

    __ev_bb_vs_1r_total_50bb

--- LeakfinderStatsService.js
+++ LeakfinderStatsService.js
@@ -320,29 +320,53 @@ INNER JOIN tourney_blinds ON tourney_blinds.id_blinds = tourney_hand_player_stat
 
     async __ev_bb_vs_1r_total_50bb() {
         this.res.write("__ev_bb_vs_1r_total_50bb")
-        let a = await this.DB.query(`SELECT (CASE WHEN ActionOpportunities = 0 THEN 0 ELSE (CAST(ActionCount AS REAL)/(ActionOpportunities)*100) END) AS result
-                                FROM(
-                                SELECT(SUM(CASE WHEN(
-                                substring(THS.str_aggressors_p from 2 for 1) = substring(THS.str_actors_p from 1 for 1) and
-                                             (substring(THS.str_actors_p from 2 for 1) = CAST(THPS.position as VARCHAR(9)) or LA.action = 'F') and
-                                              THPS.amt_p_2bet_facing > 0 and
-                                              THPS.amt_p_3bet_facing = 0 and
-                                               THPS.amt_p_4bet_facing = 0 and THPS.amt_p_5bet_facing = 0 and
-                                                (THPS.amt_before - THPS.amt_ante) / TB.amt_bb < 50
-                                ) 
-                                                                THEN THPS.amt_expected_won / TB.amt_bb ELSE 0 END)) AS ActionCount,
-                                                                (SUM(CASE WHEN(
-                                substring(THS.str_aggressors_p from 2 for 1) = substring(THS.str_actors_p from 1 for 1) and
-                                             (substring(THS.str_actors_p from 2 for 1) = CAST(THPS.position as VARCHAR(9)) or LA.action = 'F') and
-                                              THPS.amt_p_2bet_facing > 0 and
-                                              THPS.amt_p_3bet_facing = 0 and
-                                               THPS.amt_p_4bet_facing = 0 and THPS.amt_p_5bet_facing = 0 and
-                                                (THPS.amt_before - THPS.amt_ante) / TB.amt_bb < 50
-                                ) 
-                                THEN 1 ELSE 0 END)) AS ActionOpportunities ${this.check2_str}`);
-        let result = a.rows[0]["result"];
+        let a = await this.DB.query(`
+            SELECT SUM(tourney_hand_player_statistics.amt_expected_won / tourney_blinds.amt_bb)
+            FROM tourney_hand_player_statistics
+                     INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
+                     INNER JOIN tourney_hand_summary
+                                ON tourney_hand_player_statistics.id_hand = tourney_hand_summary.id_hand
+                     INNER JOIN lookup_actions ON tourney_hand_player_statistics.id_action_p = lookup_actions.id_action
+            WHERE ${this.check_str}
+              AND SUBSTRING(tourney_hand_summary.str_aggressors_p FROM 2 FOR 1) =
+                  SUBSTRING(tourney_hand_summary.str_actors_p FROM 1 FOR 1)
+              AND (SUBSTRING(tourney_hand_summary.str_actors_p FROM 2 FOR 1) =
+                   CAST(tourney_hand_player_statistics.position AS VARCHAR(9)) OR
+                   lookup_actions.action = 'F')
+              AND tourney_hand_player_statistics.amt_p_2bet_facing > 0
+              AND tourney_hand_player_statistics.amt_p_3bet_facing = 0
+              AND tourney_hand_player_statistics.amt_p_4bet_facing = 0
+              AND tourney_hand_player_statistics.amt_p_5bet_facing = 0
+              AND (tourney_hand_player_statistics.amt_before - tourney_hand_player_statistics.amt_ante) /
+                  tourney_blinds.amt_bb < 50
+        `);
+
+        let b = await this.DB.query(`
+            SELECT COUNT(*)
+            FROM tourney_hand_player_statistics
+                     INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
+                     INNER JOIN tourney_hand_summary
+                                ON tourney_hand_player_statistics.id_hand = tourney_hand_summary.id_hand
+                     INNER JOIN lookup_actions ON tourney_hand_player_statistics.id_action_p = lookup_actions.id_action
+            WHERE ${this.check_str}
+              AND SUBSTRING(tourney_hand_summary.str_aggressors_p FROM 2 FOR 1) =
+                  SUBSTRING(tourney_hand_summary.str_actors_p FROM 1 FOR 1)
+              AND (SUBSTRING(tourney_hand_summary.str_actors_p FROM 2 FOR 1) =
+                   CAST(tourney_hand_player_statistics.position AS VARCHAR(9)) OR
+                   lookup_actions.action = 'F')
+              AND tourney_hand_player_statistics.amt_p_2bet_facing > 0
+              AND tourney_hand_player_statistics.amt_p_3bet_facing = 0
+              AND tourney_hand_player_statistics.amt_p_4bet_facing = 0
+              AND tourney_hand_player_statistics.amt_p_5bet_facing = 0
+              AND (tourney_hand_player_statistics.amt_before - tourney_hand_player_statistics.amt_ante) /
+                  tourney_blinds.amt_bb < 50
+        `);
+
+        let result = (a.rows[0].count / b.rows[0].count) * 100;
         this.data['__ev_bb_vs_1r_total_50bb'] = isNaN(result) ? 0 : result;
-        this.formulas['__ev_bb_vs_1r_total_50bb'] = `${a.rows[0]["result"]}`;
+        this.formulas['__ev_bb_vs_1r_total_50bb'] = `${a.rows[0].count} / ${b.rows[0].count}`;
     }
 
     async count_torney() {
@@ -12054,6 +12078,7 @@ WHERE ${this.check_str}`);
         INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_open_opp 
@@ -12068,6 +12093,7 @@ WHERE ${this.check_str}`);
         INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_open_opp 
@@ -12088,6 +12114,7 @@ WHERE ${this.check_str}`);
         INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_open_opp 
@@ -12102,6 +12129,7 @@ WHERE ${this.check_str}`);
         INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_open_opp 
@@ -12122,6 +12150,7 @@ WHERE ${this.check_str}`);
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_limp 
@@ -12136,6 +12165,7 @@ WHERE ${this.check_str}`);
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_limp 
@@ -12157,6 +12187,7 @@ WHERE ${this.check_str}`);
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_limp 
@@ -12171,6 +12202,7 @@ WHERE ${this.check_str}`);
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.flg_p_limp 
@@ -12193,6 +12225,7 @@ WHERE ${this.check_str}`);
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.position = 8 
@@ -12208,6 +12241,7 @@ WHERE ${this.check_str}`);
         INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
 		INNER JOIN lookup_actions ON id_action = tourney_hand_player_statistics.id_action_p
 		INNER JOIN tourney_hand_summary ON tourney_hand_summary.id_hand = tourney_hand_player_statistics.id_hand
+        INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
         WHERE
         ${this.check_str}
         AND tourney_hand_player_statistics.position = 8 
