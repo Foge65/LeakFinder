a0368287ac62f5c2e238920ab1fd016c6832500d
Foge65 <mr.texnik666@gmail.com>
2023-08-13 21:17:39 +0300

    Try fix Preflop_3Max_BB_vs_BTN_OR_less_2_2bb_Call

--- LeakfinderStatsServiceSpinSimple.js
+++ LeakfinderStatsServiceSpinSimple.js
@@ -1449,12 +1449,9 @@ class Stats {
               AND tourney_hand_player_statistics.cnt_players = 3
               AND tourney_hand_player_statistics.position = 8
               AND tourney_hand_player_statistics.flg_p_face_raise
+              AND tourney_hand_summary.str_actors_p = '08'
               AND tourney_hand_player_statistics.amt_p_raise_facing / tourney_hand_player_statistics.amt_blind <= 2.2
-              AND NOT (tourney_hand_player_statistics.enum_face_allin = 'P'
-                OR tourney_hand_player_statistics.enum_face_allin = 'p')
-              AND NOT tourney_hand_player_statistics.flg_p_4bet_opp
-              AND NOT tourney_hand_player_statistics.flg_p_squeeze_opp
-              AND SUBSTRING(tourney_hand_summary.str_aggressors_p FROM 2 FOR 1) = '0'
+              AND NOT tourney_hand_player_statistics.enum_face_allin ILIKE 'P'
               AND LA_P.action = 'C'
         `);
 
@@ -1468,12 +1465,11 @@ class Stats {
               AND tourney_hand_player_statistics.cnt_players = 3
               AND tourney_hand_player_statistics.position = 8
               AND tourney_hand_player_statistics.flg_p_face_raise
+              AND substring(tourney_hand_summary.str_actors_p FROM 1 FOR 1) = '0'
+              AND (substring(tourney_hand_summary.str_actors_p FROM 2 FOR 1) = '8'
+                OR substring(tourney_hand_summary.str_actors_p FROM 2 FOR 1) = '')
               AND tourney_hand_player_statistics.amt_p_raise_facing / tourney_hand_player_statistics.amt_blind <= 2.2
-              AND NOT (tourney_hand_player_statistics.enum_face_allin = 'P'
-                OR tourney_hand_player_statistics.enum_face_allin = 'p')
-              AND NOT tourney_hand_player_statistics.flg_p_4bet_opp
-              AND NOT tourney_hand_player_statistics.flg_p_squeeze_opp
-              AND SUBSTRING(tourney_hand_summary.str_aggressors_p FROM 2 FOR 1) = '0'
+              AND NOT tourney_hand_player_statistics.enum_face_allin ILIKE 'P'
         `);
 
         let result = (a.rows[0].count / b.rows[0].count) * 100;
