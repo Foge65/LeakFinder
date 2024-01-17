c7a48b9f5303366f605af0552e73a7fbe4ac719b
Foge65 <mr.texnik666@gmail.com>
2024-01-17 10:11:15 +0300

    Preflop_HU_SB_Limp_Fold_vs_ISO_AI

--- LeakfinderStatsServiceSpinSimple.js
+++ LeakfinderStatsServiceSpinSimple.js
@@ -2932,14 +2932,14 @@ class Stats {
             SELECT COUNT(*)
             FROM tourney_hand_player_statistics
                      INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
                      INNER JOIN lookup_actions AS LA_P ON tourney_hand_player_statistics.id_action_p = LA_P.id_action
             WHERE ${this.check_str}
               AND tourney_hand_player_statistics.cnt_players = 2
               AND tourney_hand_player_statistics.position = 9
               AND tourney_hand_player_statistics.flg_p_limp
-              AND NOT tourney_hand_player_statistics.enum_allin ILIKE 'P'
-              AND tourney_hand_player_statistics.amt_p_raise_facing /
-                  tourney_hand_player_statistics.amt_p_effective_stack > 0.4
+              AND tourney_hand_player_statistics.amt_p_raise_facing + tourney_blinds.amt_bb =
+                  tourney_hand_player_statistics.amt_p_effective_stack
               AND LA_P.action = 'CF'
         `);
 
@@ -2947,16 +2947,13 @@ class Stats {
             SELECT COUNT(*)
             FROM tourney_hand_player_statistics
                      INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
-                     INNER JOIN lookup_actions AS LA_P ON tourney_hand_player_statistics.id_action_p = LA_P.id_action
+                     INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
             WHERE ${this.check_str}
               AND tourney_hand_player_statistics.cnt_players = 2
               AND tourney_hand_player_statistics.position = 9
               AND tourney_hand_player_statistics.flg_p_limp
-              AND NOT tourney_hand_player_statistics.enum_allin ILIKE 'P'
-              AND tourney_hand_player_statistics.amt_p_raise_facing /
-                  tourney_hand_player_statistics.amt_p_effective_stack > 0.4
-              AND (LA_P.action = 'CF'
-                OR LA_P.action = 'CC')
+              AND tourney_hand_player_statistics.amt_p_raise_facing + tourney_blinds.amt_bb =
+                  tourney_hand_player_statistics.amt_p_effective_stack
         `);
 
         let result = (a.rows[0].count / b.rows[0].count) * 100;
