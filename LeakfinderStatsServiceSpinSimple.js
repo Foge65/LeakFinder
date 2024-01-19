f17bbe69b321694f410f01fb8b7b24d483a6d19b
Foge65 <mr.texnik666@gmail.com>
2024-01-19 20:21:58 +0300

    Preflop_HU_SB_Limp_Call_vs_ISO_NAI

--- LeakfinderStatsServiceSpinSimple.js
+++ LeakfinderStatsServiceSpinSimple.js
@@ -2968,12 +2968,13 @@ class Stats {
             SELECT COUNT(*)
             FROM tourney_hand_player_statistics
                      INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
                      INNER JOIN lookup_actions AS LA_P ON tourney_hand_player_statistics.id_action_p = LA_P.id_action
             WHERE ${this.check_str}
               AND tourney_hand_player_statistics.cnt_players = 2
               AND tourney_hand_player_statistics.position = 9
               AND tourney_hand_player_statistics.flg_p_limp
-              AND tourney_hand_player_statistics.amt_p_raise_facing /
+              AND (tourney_hand_player_statistics.amt_p_raise_facing + 1 * tourney_blinds.amt_bb) /
                   tourney_hand_player_statistics.amt_p_effective_stack < 0.4
               AND LA_P.action = 'CC'
         `);
@@ -2982,12 +2983,13 @@ class Stats {
             SELECT COUNT(*)
             FROM tourney_hand_player_statistics
                      INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_blinds ON tourney_hand_player_statistics.id_blinds = tourney_blinds.id_blinds
                      INNER JOIN lookup_actions AS LA_P ON tourney_hand_player_statistics.id_action_p = LA_P.id_action
             WHERE ${this.check_str}
               AND tourney_hand_player_statistics.cnt_players = 2
               AND tourney_hand_player_statistics.position = 9
               AND tourney_hand_player_statistics.flg_p_limp
-              AND tourney_hand_player_statistics.amt_p_raise_facing /
+              AND (tourney_hand_player_statistics.amt_p_raise_facing + 1 * tourney_blinds.amt_bb) /
                   tourney_hand_player_statistics.amt_p_effective_stack < 0.4
               AND LA_P.action SIMILAR TO 'C(F|C|R)'
         `);
