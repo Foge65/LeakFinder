43466f112d31fe5e8f8259951f00d5c29754a36b
Foge65 <mr.texnik666@gmail.com>
2024-01-31 20:27:12 +0300

    Postflop_Defence_OOP_HU_Fold_vs_B_X_B

--- LeakfinderStatsServiceSpin.js
+++ LeakfinderStatsServiceSpin.js
@@ -12652,4 +12652,46 @@ class Stats {
         this.data['Postflop_Defence_OOP_HU_Donk_River'] = isNaN(result) ? 0 : result;
         this.formulas['Postflop_Defence_OOP_HU_Donk_River'] = `${a.rows[0].count} / ${b.rows[0].count}`;
     }
+
+    async Postflop_Defence_OOP_HU_Fold_vs_B_X_B() {
+        if (this.res) this.res.write("Postflop_Defence_OOP_HU_Fold_vs_B_X_B")
+
+        let a = await this.DB.query(`
+            SELECT COUNT(*)
+            FROM tourney_hand_player_statistics
+                     INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN lookup_actions AS LA_P ON tourney_hand_player_statistics.id_action_p = LA_P.id_action
+                     INNER JOIN lookup_actions AS LA_F ON tourney_hand_player_statistics.id_action_f = LA_F.id_action
+                     INNER JOIN lookup_actions AS LA_T ON tourney_hand_player_statistics.id_action_t = LA_T.id_action
+                     INNER JOIN lookup_actions AS LA_R ON tourney_hand_player_statistics.id_action_r = LA_R.id_action
+            WHERE ${this.check_str}
+              AND tourney_hand_player_statistics.cnt_players = 2
+              AND tourney_hand_player_statistics.position = 8
+              AND LA_P.action SIMILAR TO 'X|C'
+              AND LA_F.action LIKE '%C'
+              AND LA_T.action = 'X'
+              AND LA_R.action = 'XF'
+        `);
+
+        let b = await this.DB.query(`
+            SELECT COUNT(*)
+            FROM tourney_hand_player_statistics
+                     INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN lookup_actions AS LA_P ON tourney_hand_player_statistics.id_action_p = LA_P.id_action
+                     INNER JOIN lookup_actions AS LA_F ON tourney_hand_player_statistics.id_action_f = LA_F.id_action
+                     INNER JOIN lookup_actions AS LA_T ON tourney_hand_player_statistics.id_action_t = LA_T.id_action
+                     INNER JOIN lookup_actions AS LA_R ON tourney_hand_player_statistics.id_action_r = LA_R.id_action
+            WHERE ${this.check_str}
+              AND tourney_hand_player_statistics.cnt_players = 2
+              AND tourney_hand_player_statistics.position = 8
+              AND LA_P.action SIMILAR TO 'X|C'
+              AND LA_F.action LIKE '%C'
+              AND LA_T.action = 'X'
+              AND tourney_hand_player_statistics.amt_r_bet_facing > 0
+        `);
+
+        let result = (a.rows[0].count / b.rows[0].count) * 100;
+        this.data['Postflop_Defence_OOP_HU_Fold_vs_B_X_B'] = isNaN(result) ? 0 : result;
+        this.formulas['Postflop_Defence_OOP_HU_Fold_vs_B_X_B'] = `${a.rows[0].count} / ${b.rows[0].count}`;
+    }
 }
