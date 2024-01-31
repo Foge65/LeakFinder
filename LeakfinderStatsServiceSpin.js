b6d52ac13130284db42328fb4aaf5a40e3cc24f2
Foge65 <mr.texnik666@gmail.com>
2024-01-31 23:01:19 +0300

    Postflop_Attack_IP_BTN_Bet_Turn

--- LeakfinderStatsServiceSpin.js
+++ LeakfinderStatsServiceSpin.js
@@ -12931,4 +12931,41 @@ class Stats {
         this.data['Postflop_Attack_IP_BTN_Bet_Flop'] = isNaN(result) ? 0 : result;
         this.formulas['Postflop_Attack_IP_BTN_Bet_Flop'] = `${a.rows[0].count} / ${b.rows[0].count}`;
     }
+
+    async Postflop_Attack_IP_BTN_Bet_Turn() {
+        if (this.res) this.res.write("Postflop_Attack_IP_BTN_Bet_Turn")
+
+        let a = await this.DB.query(`
+            SELECT COUNT(*)
+            FROM tourney_hand_player_statistics
+                     INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_hand_summary
+                                ON tourney_hand_player_statistics.id_hand = tourney_hand_summary.id_hand
+                     INNER JOIN lookup_actions AS LA_T ON tourney_hand_player_statistics.id_action_t = LA_T.id_action
+            WHERE ${this.check_str}
+              AND tourney_hand_player_statistics.cnt_players = 3
+              AND tourney_hand_player_statistics.position = 0
+              AND tourney_hand_summary.cnt_players_t = 2
+              AND LA_T.action LIKE 'B%'
+        `);
+
+        let b = await this.DB.query(`
+            SELECT COUNT(*)
+            FROM tourney_hand_player_statistics
+                     INNER JOIN player ON tourney_hand_player_statistics.id_player = player.id_player
+                     INNER JOIN tourney_hand_summary
+                                ON tourney_hand_player_statistics.id_hand = tourney_hand_summary.id_hand
+                     INNER JOIN lookup_actions AS LA_T ON tourney_hand_player_statistics.id_action_t = LA_T.id_action
+            WHERE ${this.check_str}
+              AND tourney_hand_player_statistics.cnt_players = 3
+              AND tourney_hand_player_statistics.position = 0
+              AND tourney_hand_summary.cnt_players_t = 2
+              AND tourney_hand_player_statistics.amt_t_bet_facing = 0
+              AND LA_T.id_action != 0
+        `);
+
+        let result = (a.rows[0].count / b.rows[0].count) * 100;
+        this.data['Postflop_Attack_IP_BTN_Bet_Turn'] = isNaN(result) ? 0 : result;
+        this.formulas['Postflop_Attack_IP_BTN_Bet_Turn'] = `${a.rows[0].count} / ${b.rows[0].count}`;
+    }
 }
